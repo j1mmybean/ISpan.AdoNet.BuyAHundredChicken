2214c051ee1e0de1af12087fc1968b7bf594b2de
j1mmybean <carb4305@gmail.com>
2023-01-05 16:40:29 +0800

    1/5

--- HundredChicken.ConsoleApp/Program.cs
+++ HundredChicken.ConsoleApp/Program.cs
@@ -15,24 +15,44 @@ namespace HundredChicken.ConsoleApp
 		//CockAmount + HenAmount + ChickAmount = 100
 		//CockAmount*CockPrice + HenAmount*HenPrice + ChickAmount*ChickPrice = 100
 		//i.e. CockAmount*5 + HenAmount*3 + ChickAmount*(1/3) = 100
-		//i.e. CockAmount*15 + HenAmount*9 + ChickAmount = 300
-		//Thus, CockAmount*14 + HenAmount*8 = 200
+
 		static void Main(string[] args)
         {
-            for (int CockAmount = 0; CockAmount < 14; CockAmount++)//15*14 = 210 > 200
-            {
-                for (int HenAmount = 0; HenAmount <= 25; HenAmount++)//25*8 = 200
-                {
-                    if (CockAmount * 14 + HenAmount * 8 == 200)
-                    {
-                        int ChickAmount = 100 - CockAmount - HenAmount;//小雞數量=100-公雞數量-母雞數量
-                        Console.WriteLine($"公雞{CockAmount}隻," +
-                                          $"母雞{HenAmount}隻," +
-                                          $"小雞{ChickAmount}隻");
-                        break;//CockAmount為常數時,聯立方程為線性獨立,有唯一解
-                    }
-                }
-            }
+
         }
 	}
+
+    public class CountChicken
+    {
+		int cock;
+		int hen;
+		int chick;
+
+		int cockPrice = 5;
+		int henPrice = 3;
+		decimal chickPrice = 1/3;
+
+        public int ChickenPossibiltiy()
+        {
+			for (int CockAmount = 0; CockAmount < 14; CockAmount++)//15*14 = 210 > 200
+			{
+				for (int HenAmount = 0; HenAmount <= 25; HenAmount++)//25*8 = 200
+				{
+					int ChickAmount = 100 - CockAmount - HenAmount;//小雞數量=100-公雞數量-母雞數量
+
+					if (CockAmount * 14 + HenAmount * 8 == 200)
+					{
+						Console.WriteLine($"公雞{CockAmount}隻," +
+										  $"母雞{HenAmount}隻," +
+										  $"小雞{ChickAmount}隻");
+						break;//CockAmount為常數時,聯立方程為線性獨立,有唯一解
+					}
+				}
+			}
+			return 
+		}
+    }
+
+
+
 }
